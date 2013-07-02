b3f7ca3bb87350550a9926491d6c9560ab16c5ae
doomy <vladimir.bartek@gmail.com>
2013-07-02 11:58:45 +0200

    Show list of tasks at tasklist controller

--- db_calls/tasklist.php
+++ db_calls/tasklist.php
@@ -1,8 +1,13 @@
 <?php
 
-class TaskListDbCalls extends BasePackageWithDb {
-    public function run() {
-        $sql = "SELECT name FROM t_tasks"
+class TaskList_db_calls extends BasePackageWithDb {
+    public function get_task_names() {
+        $sql = "SELECT task FROM t_tasks;";
+        $result = mysql_query($sql);
+        while ($row = mysql_fetch_object($result)) {
+            $names[] = $row->task;
+        }
+        return $names;
     }
 }
 
--- lib/app/TaskListController.php
+++ lib/app/TaskListController.php
@@ -1,7 +1,12 @@
 <?php
     class TaskListController extends BasePackageWithDb {
+    
         public function run() {
-            echo "Tasklist Controller runs!";
+            $names = $this->dbh->run_db_call("Tasklist", "get_task_names");
+            echo "<ul>";
+            foreach ($names as $name)
+                echo "<li>$name</li>";
+            echo "</ul>";
         }
     }
 ?>
